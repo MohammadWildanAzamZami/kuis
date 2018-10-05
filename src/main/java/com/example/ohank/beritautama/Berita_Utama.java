fc3c8e0afa71d13d464d07f11e318b304bfe9ebd
MohammadWildanAzamZami <43130118+MohammadWildanAzamZami@users.noreply.github.com>
2018-10-05 13:26:44 +0700

    Add files via upload

---
+++ src/main/java/com/example/ohank/beritautama/Berita_Utama.java
@@ -0,0 +1,13 @@
+package com.example.ohank.beritautama;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Berita_Utama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_berita__utama);
+    }
+}
